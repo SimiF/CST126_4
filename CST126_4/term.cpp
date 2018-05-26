f0fcb4d1812b255884cc8483660100dc3976762f
SimiF <SimiF@users.noreply.github.com>
2018-05-25 20:30:54 -0700

    being worked on.

--- CST126_4/term.cpp
+++ CST126_4/term.cpp
@@ -1,2 +1,18 @@
 #include "stdafx.h"
 #include "term.h"
+#include "iostream"
+#include "iomanip"
+
+void term_data::print_term_data()
+{
+	std::cout << std::right << std::setw(11) << name << ":" << std::left 
+		      << std::setw(2) << definition << std::endl;
+}
+
+void term_data::search_term(std::string n)
+{
+	for (int i{ 10 }; i <= 10; i++)
+	{
+
+	}
+}
--- CST126_4/term.h
+++ CST126_4/term.h
@@ -1,4 +1,5 @@
 #include "stdafx.h"
+#include "string"
 #pragma once
 /* according to https://en.wikipedia.org/wiki/Pragma_once pragma once does the same job as the include
 directives but with advantages being that it's less code and potentially faster */
@@ -6,10 +7,21 @@ directives but with advantages being that it's less code and potentially faster 
 class term_data
 {
 public:
+	term_data() { name = ""; definition = ""; year_used = 0; };
+	term_data(std::string n, std::string def, int year) { name = n; definition = def; year_used = year; };
+	void print_term_data();
+	void search_term(std::string n);
 private:
+	std::string name;
+	std::string definition;
+	int year_used;
+	term_data* next;
 };
 
-struct node
+class node
 {
-
+public:
+	void add_term();
+	void print_term_info();
+	void search_term();
 };
